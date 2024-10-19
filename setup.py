f17d2c82e6da9ebf9daa993ff825218dcf6f2a03
Barry Baker <bbakernoaa@users.noreply.github.com>
2024-10-19 02:08:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='ReMoNet',
+    version='0.1.0',
+    description='ReMoNet: Regridding Tools for xarray',
+    author='bbakernoaa',
+    author_email='your-email@example.com',
+    url='https://github.com/bbakernoaa/ReMONET',
+    packages=find_packages(),
+    install_requires=[
+        'xarray',
+        'numpy',
+        'scipy',
+        'dask'
+    ],
+    extras_require={
+        'cf': ['cf_xarray']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
